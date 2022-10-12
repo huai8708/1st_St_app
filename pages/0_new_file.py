87e79f670fc43a6fb73732bb056e3c10d0a9440f
huai8708 <huai8708@163.com>
2022-10-12 00:29:34 +0000

    test

--- pages/0_new_file.py
+++ pages/0_new_file.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.title("this is test")
+
+c1,c2 = st.columns(2)
+b1 = c1.button("c1")
+b2 = c2.button("c2")
+
+if b1:
+    st.write("this is c1")
+
+if b2 :
+    st.write("this is c2")
