09520bffca8783a73d7a5b94903ca39c126fd8f1
huai8708 <huai8708@163.com>
2022-10-12 02:04:43 +0000

    add mod

--- pages/0_new_file.py
+++ pages/0_new_file.py
@@ -1,6 +1,20 @@
 import streamlit as st
 st.title("this is test")
 
+st.write(
+    """
+    ✨ Streamlit Elements &nbsp; [![GitHub][github_badge]][github_link] [![PyPI][pypi_badge]][pypi_link]
+    =====================
+    Create a draggable and resizable dashboard in Streamlit, featuring Material UI widgets,
+    Monaco editor (Visual Studio Code), Nivo charts, and more!
+    [github_badge]: https://badgen.net/badge/icon/GitHub?icon=github&color=black&label
+    [github_link]: https://github.com/okld/streamlit-elements
+    [pypi_badge]: https://badgen.net/pypi/v/streamlit-elements?icon=pypi&color=black&label
+    [pypi_link]: https://pypi.org/project/streamlit-elements
+    """
+)
+
+
 c1,c2 = st.columns(2)
 b1 = c1.button("c1")
 b2 = c2.button("c2")
